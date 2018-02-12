86d99a016e03a946214262c62b8667482afc2047
João Gradim <joao.gradim@gmail.com>
2018-02-12 18:51:04 +0000

    Add Abyssal to sponsor list

--- src/components/landing/sponsors.js
+++ src/components/landing/sponsors.js
@@ -1,5 +1,6 @@
 import sponsorDoist from "assets/images/sponsor-doist.svg";
 import sponsorLoM from "assets/images/sponsor-lom.svg";
+import sponsorAbyssal from "assets/images/sponsor-abyssal.svg";
 // import sponsorJumia from "assets/images/sponsor-jumia.svg";
 import sponsorByside from "assets/images/sponsor-byside.svg";
 import sponsorHapibot from "assets/images/sponsor-hapibot.svg";
@@ -7,25 +8,21 @@ import sponsorCMP from "assets/images/sponsor-cmp.svg";
 import sponsorScaleUpPorto from "assets/images/sponsor-scale-up-porto.svg";
 
 export default [
-  // gold
   [
+    // gold
     { src: sponsorDoist , url: "https://doist.com/"     , className: "doist" , description: "Doist is a remote-first company with employees from all around the world. Our mission is to help people do more and stress less.<br /><br />Our interests and hobbies are as varied as our nationalities– and creating amazing products for people like you is what brings us all together." },
     { src: sponsorLoM   , url: "https://lifeonmars.pt/" , className: "lom"   , description: "Life on Mars is an elite software consultancy based in Germany and Portugal. We balance a strong engineering culture with a set of agile processes. Our clients range from early-stage startups to large companies such as Twitter, under engagement types ranging from team extensions to emergency incident support." },
+
+    // silver
+    { src: sponsorAbyssal , url: "https://abyssal.eu/home/", className: "abyssal", description: "Abyssal develops integrated Subsea Navigation Solutions for Remotely Operated Vehicles (ROVs) in the Oil & Gas, Marine Renewables and Underwater Mining industries.<br /><br />Our aim is to enable offshore and onshore O&G personnel to obtain relevant and accurate real-time information and to perform safer and more efficient ROV Operations thus decreasing the likelihood of incidents, accidents and associated costs.<br /><br /> Backed by our close relationships with the industry, the expertise of our team, the continuous R&D activities and our commitment to provide our customers “vision in depth”, we are able to provide state of the art solutions." },
   ],
 
-  // // silver
-  // [
-  //   // ABYSSAL
-  // ],
-
-  // bronze
   [
+    // bronze
     { src: sponsorByside  , url: "http://www.byside.com/"   , className: "byside"  , description: null },
     { src: sponsorHapibot , url: "https://www.hapibot.com/" , className: "hapibot" , description: null },
-  // ],
 
-  // copper
-  // [
+    // copper
     // { src: sponsorJumia, url: "https://group.jumia.com/", className: "jumia", description: null },
   ],
 
@@ -35,3 +32,6 @@ export default [
     { src: sponsorCMP          , url: "http://www.cm-porto.pt/" , className: "cmp"      , description: null },
   ],
 ];
+
+
+
