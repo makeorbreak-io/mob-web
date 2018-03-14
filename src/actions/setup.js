0e6548231ac5c8c134852c9652e8053c1b81170d
João Gradim <joao.gradim@gmail.com>
2018-03-14 15:47:33 +0000

    Re-introduce notifications

--- src/actions/setup.js
+++ src/actions/setup.js
@@ -1,11 +1,12 @@
 import { createAction } from "redux-actions";
+import { parse, isBefore } from "date-fns";
 // import { each, isEmpty } from "lodash";
 
 //
 // Redux
 // import { refreshCurrentUser } from "actions/current_user";
 // import { logout } from "actions/authentication";
-// import { addNotification } from "actions/notifications";
+import { addNotification } from "actions/notifications";
 
 //
 // Constants
@@ -24,4 +25,16 @@ import {
 // Actions
 export const setReady = createAction(SET_READY);
 
-export const performSetup = () => (dispatch) => dispatch(setReady(true));
+export const performSetup = () => (dispatch) => {
+
+  // MoB session March 15, 2018
+  if (isBefore(new Date(), parse("2018-03-15T19:00:00"))) {
+    dispatch(addNotification({
+      title: "MoB Session, March 15th, 18:00",
+      message: "Don't miss our first ever MoB Session, March 15, at Blip. Doors open at 6p.m. and entrance is free (no application needed).\nMore info at <link>https://www.facebook.com/events/154653598682051</link>",
+      link: "https://www.facebook.com/events/154653598682051",
+    }));
+  }
+
+  dispatch(setReady(true));
+};
--- src/lib/notifications.js
+++ src/lib/notifications.js
@@ -1,3 +1,5 @@
 export const linkedMessage = (message, link) => {
-  return message.replace(/<link>(.*)<\/link>/g, `<a href="${link}">$1</a>`);
+  return message
+    .replace("\n", "<br />")
+    .replace(/<link>(.*)<\/link>/g, `<a href="${link}" target="_blank" rel="noopener noreferrer">$1</a>`);
 };
