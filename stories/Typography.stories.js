1d9b339aa077d7c325e70b2e3299efcb495e8235
João Gradim <joao.gradim@gmail.com>
2020-01-30 11:27:56 +0000

    Fix stories

--- stories/Typography.stories.js
+++ stories/Typography.stories.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import { Heading, Link, P } from "components/2020/uikit";
+import {
+  Heading,
+  Link,
+  P,
+  Section
+} from "components/2020/uikit";
 
 export default { title: "Typography" };
 
 const colors = (size, centered = false) => (
   ["black", "white", "purple", "blue", "green"].map((color) => (
-    <section key={`${size}-${color}`} className={color === "white" ? "black" : "" }>
+    <Section key={`${size}-${color}`} background={color === "white" ? "black" : "white" }>
       <Heading {...{ size, color }}>Heading {size.toUpperCase()} {color}</Heading>
       {centered &&
         <Heading {...{ size, color }} centered>Heading {size.toUpperCase()} centered {color}</Heading>
       }
-    </section>
+    </Section>
   ))
 );
 
 export const Headings = () => (
-  <section>
+  <Section>
     <Heading>Heading Default (XL)</Heading>
     <Heading centered>Heading Default (XL) centered</Heading>
 
@@ -39,47 +44,47 @@ export const Headings = () => (
 
     <Heading size="s">Heading S</Heading>
     {colors("s")}
-  </section>
+  </Section>
 );
 
 export const Text = () => (
   <>
-    <section>
+    <Section>
       <P large>P large</P>
       <P>P default</P>
-    </section>
+    </Section>
 
-    <section className="black">
+    <Section background="black">
       <P color="white" large>P white large</P>
       <P color="white">P white default</P>
-    </section>
+    </Section>
   </>
 );
 
 export const Links = () => (
   <BrowserRouter>
-    <section>
+    <Section>
       <P>
         <Link to="/">Internal Link to /</Link>
       </P>
-    </section>
+    </Section>
 
-    <section>
+    <Section>
       <P>
         <Link to="#example">Anchor Link to #example (broken because storybook)</Link>
       </P>
-    </section>
+    </Section>
 
-    <section>
+    <Section>
       <P>
         <Link external to="https://google.com">External Link to google</Link>
       </P>
-    </section>
+    </Section>
   </BrowserRouter>
 );
 
 export const FullExample = () => (
-  <section>
+  <Section>
     <Heading size="xl" color="purple" centered>A nice title</Heading>
 
     <P large>
@@ -90,5 +95,5 @@ export const FullExample = () => (
       <br />
         Like this one does. And maybe <Link external to="https://google.com"></Link>
     </P>
-  </section>
+  </Section>
 );
